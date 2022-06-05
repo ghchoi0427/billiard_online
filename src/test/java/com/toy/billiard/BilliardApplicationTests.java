684ea4d5bb1beab4e94eaf4787fb52c9bf2fa05a
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-05 14:36:06 +0900

    Initial commit

---
+++ src/test/java/com/toy/billiard/BilliardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.billiard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
