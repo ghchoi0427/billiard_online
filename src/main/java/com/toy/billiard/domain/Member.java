ac560fafa616e38e039e579f4bec7e1c72d0d86b
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-05 22:55:28 +0900

    feat: 멤버 엔티티 생성

---
+++ src/main/java/com/toy/billiard/domain/Member.java
@@ -0,0 +1,32 @@
+package com.toy.billiard.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
