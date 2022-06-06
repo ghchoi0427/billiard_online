adc02ab0fd1ad581ca6ba380f6da9266c51dd17c
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-06 21:20:27 +0900

    fix: @CookieValue 어노테이션 사용

--- src/main/java/com/toy/billiard/controller/HomeController.java
+++ src/main/java/com/toy/billiard/controller/HomeController.java
@@ -1,24 +1,42 @@
 package com.toy.billiard.controller;
 
+import com.toy.billiard.domain.Room;
+import com.toy.billiard.dto.RoomDto;
+import com.toy.billiard.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    private final RoomService roomService;
+
+    public HomeController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
     @GetMapping("")
-    public String home(HttpServletRequest request) {
-        Cookie[] list = request.getCookies();
-        for (Cookie cookie : list) {
-            if (cookie.getName().equals("username")) {
-                System.out.println(cookie.getValue());
-            }
+    public String home(@CookieValue(name = "username", required = false) String username, Model model) {
+
+        List<RoomDto> dtos = new ArrayList<>();
+
+        for (Room r : roomService.getAllRooms()) {
+            RoomDto dto = new RoomDto();
+            dto.setId(r.getId());
+            dto.setName(r.getName());
+            dto.setJoined(r.getJoined());
+            dtos.add(dto);
         }
+        model.addAttribute("rooms", dtos);
         return "home";
     }
 }
