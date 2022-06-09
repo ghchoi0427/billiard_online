c9be962fdeade3c8f71bade4d79fb13306e15791
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-09 14:46:56 +0900

    feat: 메시지 객체 필드 추가

--- src/main/java/com/toy/billiard/domain/ChatMessage.java
+++ src/main/java/com/toy/billiard/domain/ChatMessage.java
@@ -4,13 +4,33 @@ public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+    private float dx;
+    private float dy;
+
 
     public enum MessageType {
         CHAT,
         JOIN,
         LEAVE,
         PRESENT,
+        DONE,
         SHOT
+    }
+
+    public float getDx() {
+        return dx;
+    }
+
+    public void setDx(float dx) {
+        this.dx = dx;
+    }
+
+    public float getDy() {
+        return dy;
+    }
+
+    public void setDy(float dy) {
+        this.dy = dy;
     }
 
     public MessageType getType() {
