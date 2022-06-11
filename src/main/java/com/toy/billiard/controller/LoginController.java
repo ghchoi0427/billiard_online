13d6fd4b4fd0719d7793b226e20190197d32ab17
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-12 00:51:56 +0900

    fix: request mapping 추가

--- src/main/java/com/toy/billiard/controller/LoginController.java
+++ src/main/java/com/toy/billiard/controller/LoginController.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("")
 public class LoginController {
 
     @GetMapping("/")
