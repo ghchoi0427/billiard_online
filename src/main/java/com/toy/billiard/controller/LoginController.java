759ff69aa0952cf551503f74e58ab233c316ab7f
ghchoi0427 <ghchoi0427@gmail.com>
2022-06-16 00:30:31 +0900

    fix: url 매핑 변경

--- src/main/java/com/toy/billiard/controller/LoginController.java
+++ src/main/java/com/toy/billiard/controller/LoginController.java
@@ -5,16 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("")
 public class LoginController {
 
     @GetMapping("/")
+    public String landing() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("loginDto", new LoginDto());
         return "login";
